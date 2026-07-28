8a3830b9c3871cf5e4d89c4ec7f3c96ee56d14aa
leseke <ylan.seke@outlook.fr>
2026-07-28 20:22:37 +0200

    Create IEvent interface for EventBus events
    Add IEvent interface to represent events in EventBus

--- Simulation/Chroniques.Simulation/Kernel/EventBus/IEvent.cs
+++ Simulation/Chroniques.Simulation/Kernel/EventBus/IEvent.cs
@@ -1,1 +1,9 @@
+namespace Chroniques.Simulation.Kernel.EventBus;
 
+/// <summary>
+/// Représente un événement publié sur l'EventBus.
+/// Les événements sont des messages immuables décrivant un fait survenu.
+/// </summary>
+public interface IEvent
+{
+}
