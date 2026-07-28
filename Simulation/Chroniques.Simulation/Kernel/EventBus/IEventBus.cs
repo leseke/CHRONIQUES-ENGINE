3d0d3888a9537b17f6848e836024970f760c934e
leseke <ylan.seke@outlook.fr>
2026-07-28 20:23:10 +0200

    Add IEventBus interface for event handling
    Define IEventBus interface for event communication.

--- Simulation/Chroniques.Simulation/Kernel/EventBus/IEventBus.cs
+++ Simulation/Chroniques.Simulation/Kernel/EventBus/IEventBus.cs
@@ -1,1 +1,16 @@
+namespace Chroniques.Simulation.Kernel.EventBus;
 
+/// <summary>
+/// Bus de communication entre les systèmes.
+/// </summary>
+public interface IEventBus
+{
+    void Publish<TEvent>(TEvent evt)
+        where TEvent : IEvent;
+
+    void Subscribe<TEvent>(IEventHandler<TEvent> handler)
+        where TEvent : IEvent;
+
+    void Unsubscribe<TEvent>(IEventHandler<TEvent> handler)
+        where TEvent : IEvent;
+}
