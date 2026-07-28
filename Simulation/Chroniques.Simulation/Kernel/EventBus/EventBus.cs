6f41e2eda54cc60945a815eccb9098b0c0afd076
leseke <ylan.seke@outlook.fr>
2026-07-28 20:22:13 +0200

    Implement EventBus class with publish, subscribe, unsubscribe methods

--- Simulation/Chroniques.Simulation/Kernel/EventBus/EventBus.cs
+++ Simulation/Chroniques.Simulation/Kernel/EventBus/EventBus.cs
@@ -1,1 +1,22 @@
+namespace Chroniques.Simulation.Kernel.EventBus;
 
+public sealed class EventBus : IEventBus
+{
+    public void Publish<TEvent>(TEvent evt)
+        where TEvent : IEvent
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Subscribe<TEvent>(IEventHandler<TEvent> handler)
+        where TEvent : IEvent
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Unsubscribe<TEvent>(IEventHandler<TEvent> handler)
+        where TEvent : IEvent
+    {
+        throw new NotImplementedException();
+    }
+}
