9b1b136bfb96dbc2560b3a1d903413634ad3ce8f
leseke <ylan.seke@outlook.fr>
2026-08-13 17:44:25 +0200

    Implement lineage memory generation resolver

--- Simulation/Chroniques.Simulation/Autonomy/LineageWorldMemoryGenerationResolver.cs
+++ Simulation/Chroniques.Simulation/Autonomy/LineageWorldMemoryGenerationResolver.cs
@@ -1,1 +1,19 @@
 namespace Chroniques.Simulation.Autonomy;
+
+using Chroniques.Simulation.Components;
+using Chroniques.Simulation.Kernel;
+
+public sealed class LineageWorldMemoryGenerationResolver : IWorldMemoryGenerationResolver
+{
+    private readonly string _continuityId;
+
+    public LineageWorldMemoryGenerationResolver(string continuityId)
+    {
+        if (string.IsNullOrWhiteSpace(continuityId))
+            throw new ArgumentException("continuityId");
+        _continuityId = continuityId;
+    }
+
+    public long ResolveGeneration(World world, Tick currentTick)
+        => GenerationContinuityRegistry.Get(world, _continuityId).GenerationIndex;
+}
